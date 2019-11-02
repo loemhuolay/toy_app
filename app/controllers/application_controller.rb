60a3b8edf42b610fa4e20c582180fd8efa4de306
Loemhuo <l.lay.sc.kh@gmail.com>
2019-11-02 09:03:04 +0700

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,30 @@ class ApplicationController < ActionController::Base
     def hello
       render html: "hello, world!"
     end
+
+    class UsersController < ApplicationController
+      
+      def index
+        @users = User.all
+        
+      end
     
+      def show
+      end
+    
+      def new
+      end
+    
+      def edit
+      end
+    
+      def create
+      end
+    
+      def update
+      end
+    
+      def destroy
+      end
+    end
 end
